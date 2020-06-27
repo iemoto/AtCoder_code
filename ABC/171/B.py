29efd18df491dbd551fd4037886a8bbdef566d6d
iemoto <reyryuuu@gmail.com>
2020-06-27 20:46:38 +0900

    172init

--- ABC/171/B.py
+++ ABC/171/B.py
@@ -1,13 +1,4 @@
-from itertools import combinations
-
 N, K = map(int,input().split())
-lists = combinations(range(N),K)
 s = list(map(int,input().split()))
-min_result = 1000000000000
-for li in lists:
-    total = 0
-    for l in li:
-        total += s[l]
-    min_result = min(min_result,total)
-
-print(min_result)
+s.sort()
+print(sum(s[:K]))
